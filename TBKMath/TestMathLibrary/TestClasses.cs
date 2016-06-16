d8681175b7b597c76e2d38af68c1ca5bf74ede9a
Kepler, Thomas B <tbkepler@bu.edu>
2016-06-16 22:10:52 +0000

    manual add
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/TBKMath;C6513

---
+++ TBKMath/TestMathLibrary/TestClasses.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TBKMath;
+
+namespace TestMathLibrary
+{
+    class TestAmoeba
+    {
+        public TestAmoeba()
+        {
+            double tol = 0.001;
+            am = new Amoeba();
+            am.ToleranceRequested = tol;
+            am.Function = TestFunctions.parabola;
+        }
+
+        public bool runTest()
+        {
+            am.MaxNumIterations = 1000;
+
+            List<double> point = new List<double> { 0, 0 };
+            am.Point = point;
+
+            List<double> scale = new List<double> { 0.5, 0.5 };
+            am.Scale = scale;
+
+            am.Start();
+            return true;
+        }
+
+        public List<double> argMin;
+        public TBKMath.Amoeba am;        
+    }
+
+    class TestCodeCogs
+    {
+        public TestCodeCogs() { }
+
+        public void runTest()
+        {
+            double y = TBKMath.SpecialFunctions.Psi(0.5);
+        }
+
+    }
+
+    class TestBisector
+    {
+        public TestBisector()
+        {
+            double tol = 1E-6;
+            bi = new Bisector(tol);
+            bi.func = new Bisector.FunctionDelegate(TestFunctions.xMinusLogXMinusTwo);
+        }
+
+        public void runTest()
+        {
+            double left = 1;
+            double right = 10;
+            argMin = bi.getRoot(left, right);
+        }
+        public TBKMath.Bisector bi;
+        public double argMin;
+    }
+
+    class TestMCMC
+    {
+        public TBKMath.MCMC mcmc;
+        public TestMCMC()
+        {
+            mcmc = new MCMC();
+        }
+
+        public void RunTest()
+        {
+            
+        }
+    }
+
+    class TestSemipartition
+    {
+        Semipartition<int> semipartition;
+        public TestSemipartition()
+        {
+        }
+
+        public void Test()
+        {
+            reset();
+            semipartition.PurgeSubblocks();
+            Tuple<Semipartition<int>, Partition<int>> decomposition = semipartition.Decompose();
+            Partition<int> partition = decomposition.Item1.MergeOverlappingBlocks();
+            reset();
+            semipartition.MergeAllBlocks();
+            reset();
+            partition = semipartition.MergeOverlappingBlocks();
+            Partition<int> newPartition = new Partition<int>();
+            newPartition.AddBlock(new HashSet<int>() { 21, 22, 23 });
+            Partition<int> joined = Partition<int>.Join(partition, newPartition);
+        }
+
+        private void reset()
+        {
+            semipartition = new Semipartition<int>();
+            semipartition.AddBlock(new HashSet<int>() { 1, 2});
+            semipartition.AddBlock(new HashSet<int>() { 3, 4 });
+            semipartition.AddBlock(new HashSet<int>() { 5, 6 });
+            semipartition.AddBlock(new HashSet<int>() { 6, 7 });
+            semipartition.AddBlock(new HashSet<int>() { 8, 9 });
+            semipartition.AddBlock(new HashSet<int>() { 10, 11 });
+            semipartition.AddBlock(new HashSet<int>() { 2, 3 });
+            semipartition.AddBlock(new HashSet<int>() { 2, 3 });
+            semipartition.AddBlock(new HashSet<int>() { 1 });
+        }
+        
+    }
+}
---
+++ TBKMath/TestMathLibrary/TestFunctions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestMathLibrary
+{
+    class TestFunctions
+    {
+        public static double parabola(List<double> parameters)
+        {
+            return Math.Pow(parameters[1] - 1, 2) + Math.Pow(parameters[0] + 1, 2);
+        }
+
+        public static double xMinusLogXMinusTwo(double x)
+        {
+            return x - Math.Log(x) - 2;
+        }
+    }
+
+    public class TestStatistics
+    {
+        private double[] x;
+        private double[] y;
+        private int n;
+
+        public double LogLikelihoodLR(double[] pars)
+        {
+            double alpha = pars[0];
+            double beta = pars[1];
+            double tau = Math.Exp(pars[2]);
+
+            double SS = 0;
+            for (int i = 0; i < n; i++)
+            {
+                SS += Math.Pow(y[i] - alpha - beta * x[i], 2);
+            }
+            return -0.5 * n * Math.Log(tau) - 0.5 * SS / tau;
+        }
+
+        public double LogPriorLR(double[] pars)
+        {
+            return 0;
+        }
+
+        public void LoadData(double[] x, double[] y)
+        {
+            if (x.Length != y.Length)
+            {
+                throw new ArgumentException("The x and y vectors must be the same length.");
+            }
+            n = x.Length;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
