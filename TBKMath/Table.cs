f39c044907781506ba51b0d221957019ac6da421
Kepler, Thomas B <tbkepler@bu.edu>
2016-09-05 15:33:10 +0000

    Created StringTable class, intended to supersede Table.
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/TBKMath;C6691

--- TBKMath/Table.cs
+++ TBKMath/Table.cs
@@ -225,4 +225,291 @@ namespace TBKMath
         }
     }
 
+    public class StringTable
+    {
+        private string[,] data;
+
+        private bool hasHeader;
+        public bool HasHeader
+        {
+            get { return hasHeader; }
+        }
+
+        private string[] columnNames;
+        public string[] ColumnNames
+        {
+            get
+            {
+                if (hasHeader)
+                    return columnNames;
+
+                else return null;
+            }
+        }
+
+        private Dictionary<string, int> columnIndex;
+
+        public StringTable(int nRows, int nColumns, bool hasHeader = false)
+        {
+            this.nRows = nRows;
+            this.nColumns = nColumns;
+            this.hasHeader = hasHeader;
+            if (hasHeader)
+            {
+                columnNames = new string[nColumns];
+                columnIndex = new Dictionary<string, int>();
+            }
+
+            data = new string[nRows, nColumns];
+        }
+
+        public static StringTable FromString(string tableString, char delimiter = '\t', bool hasHeader = false)
+        {
+            StringReader reader = new StringReader(tableString);
+            string sDelimiter = delimiter.ToString();
+            List<string> temp = new List<string>();
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                temp.Add(line);
+                line = reader.ReadLine();
+            }
+            reader.Close();
+
+            if (temp.Count == 0)
+            {
+                return new StringTable(0, 0, hasHeader);
+            }
+
+            int nCols = temp[0].Count(c => { return c == delimiter; }) + 1;
+            int nRows = temp.Count - (hasHeader ? 1 : 0);
+            StringTable table = new StringTable(nRows, nCols, hasHeader);
+
+            int offset = 0;
+            if (hasHeader)
+            {
+                table.columnNames = temp[0].Split(delimiter);
+                offset = 1;
+            }
+
+            string[] buffer;
+            for (int i = 0; i < table.nRows - offset; i++)
+            {
+                buffer = temp[i + offset].Split(delimiter);
+                // check to see if it is blank
+                if (!IsTrailingBlankRow(buffer))
+                {
+                    for (int j = 0; j < buffer.Length; j++)
+                    {
+                        table.data[i, j] = buffer[j];
+                    }
+                }
+                else
+                {
+                    table.nRows--;
+                }
+            }
+            return table;
+        }
+
+        public static StringTable ReadFromFile(string FileName, char delimiterAsChar = '\t', bool hasHeader = false)
+        {
+            string tableString;
+            try
+            {
+                tableString = File.ReadAllText(FileName);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error reading table.", e);
+            }
+
+            StringTable table = FromString(tableString, delimiterAsChar, hasHeader);
+            return table;
+        }
+
+        private static bool IsTrailingBlankRow(string[] buffer)
+        {
+            if (buffer[0].Length > 0)
+                return false;
+
+            for (int iCol = 1; iCol < buffer.Length; iCol++)
+            {
+                if (buffer[iCol] != null)
+                    return false;
+            }
+            return true;
+        }
+
+        public string this[int iRow, int iCol]
+        {
+            get 
+            {
+                if (iRow < 0 | iRow > nRows | iCol < 0 | iCol > nColumns)
+                {
+                    throw new IndexOutOfRangeException("Index out range.");
+                }
+
+                return data[iRow, iCol];
+            }
+            set
+            {
+                if (iRow < 0 | iRow > nRows | iCol < 0 | iCol > nColumns)
+                {
+                    throw new IndexOutOfRangeException("Index out range.");
+                }
+
+                data[iRow, iCol] = value;
+            }
+        }
+
+        public string[] this[string columnName]
+        {
+            get
+            {
+                if (!hasHeader)
+                    return null;
+
+                if (!columnIndex.ContainsKey(columnName))
+                    return null;
+
+                string[] returnValue = new string[nRows];
+                for (int iRow = 0; iRow < nRows; iRow++) 
+                {
+                    returnValue[iRow] = data[iRow, columnIndex[columnName]];
+                }
+                return returnValue;
+            }
+
+            set
+            {
+                if (value.Length != nRows)
+                {
+                    throw new Exception("Length of vector does not equal number of rows in table.");
+                }
+
+                if (!hasHeader)
+                {
+                    throw new Exception("This table does not have column names.");
+                }
+
+                if (!columnIndex.ContainsKey(columnName))
+                { 
+                    throw new Exception("That column name is not found in this table.");
+                }
+
+                for (int iRow = 0; iRow < nRows; iRow++)
+                {
+                    data[iRow, columnIndex[columnName]] = value[iRow];
+                }
+            }
+        }
+
+        public string this[string columnName, int iRow]
+        {
+            get
+            {
+                if (!hasHeader)
+                    return null;
+
+                if (!columnIndex.ContainsKey(columnName))
+                    return null;
+
+                if (iRow < 0 | iRow > nRows)
+                {
+                    throw new IndexOutOfRangeException("Index out of range.");
+                }
+
+                return data[iRow, columnIndex[columnName]];
+            }
+
+            set
+            {
+                if (value.Length != nRows)
+                {
+                    throw new Exception("Length of vector does not equal number of rows in table.");
+                }
+
+                if (!hasHeader)
+                {
+                    throw new Exception("This table does not have column names.");
+                }
+
+                if (!columnIndex.ContainsKey(columnName))
+                {
+                    throw new Exception("That column name is not found in this table.");
+                }
+
+                data[iRow, columnIndex[columnName]] = value;
+            }
+        }
+
+        private int nRows;
+        public int NRows
+        {
+            get { return nRows; }
+        }
+
+        private int nColumns;
+        public int NColumns
+        {
+            get { return nColumns; }
+        }
+
+        public string[] Row(int i)
+        {
+            if (data == null)
+                return null;
+
+            string[] row = new string[nColumns];
+            for (int j = 0; j < nColumns; j++)
+            {
+                row[j] = data[i, j];
+            }
+            return row;
+        }
+
+        public string[] Column(int i)
+        {
+            if (data == null)
+                return null;
+
+            string[] column = new string[nRows];
+            for (int j = 0; j < nRows; j++)
+            {
+                column[j] = data[j, i];
+            }
+            return column;
+        }
+
+        public override string ToString()
+        {
+            if (NRows == 0 | nColumns == 0)
+                return "";
+
+            StringBuilder sb = new StringBuilder();
+            int offset = 0;
+            if (hasHeader)
+            {
+                offset = 1;
+                sb.Append(columnNames[0]);
+                for (int i = 0; i < nColumns; i++)
+                {
+                    sb.Append("\t" + columnNames[i]);
+                }
+                sb.AppendLine();
+            }
+            for (int iRow = offset; iRow < nRows; iRow++)
+            {
+                sb.Append(data[iRow, 0]);
+                for (int iCol = 1; iCol < nColumns; iCol++)
+                {
+                    sb.Append("\t" + data[iRow, iCol]);
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+
 }
