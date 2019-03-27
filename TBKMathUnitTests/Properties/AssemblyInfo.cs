2aa42644fecbfcb9c6c3c175af84519722e3bf82
Thomas Kepler <tbkepler@bu.edu>
2019-03-27 07:43:32 -0400

    Added Unit tests

---
+++ TBKMathUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TBKMathUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Boston University")]
+[assembly: AssemblyProduct("TBKMathUnitTests")]
+[assembly: AssemblyCopyright("Copyright © Boston University 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9e95e85-8c44-4813-b07b-7468a4f42ea0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TBKMathUnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TBKMath;
+
+namespace TBKMathUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int nTests = 4;
+            int[] k = new int[] { 3, 3, 3, 3 };
+            int[] i = new int[] { 3, 4, 5, 6 };
+            int[] j = new int[] { 4, 5, 6, 7 };
+            int[] x = new int[] { 1, -1, -1, 1 };
+
+            for (int test = 0; test < nTests; test++)
+            {
+                int y = Utilities.DesignSign(i[test], j[test], k[test]);
+                Assert.AreEqual(x[test], y);
+            }
+        }
+    }
+}
